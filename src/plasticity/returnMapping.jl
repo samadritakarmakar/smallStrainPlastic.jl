3e2d9ecf90b203bdb9da07c485c6ff326f4765d2
Samadrita Karmakar <samadritakarmakar@gmail.com>
2020-11-25 16:57:30 +0100

    Some testing still problems persist!

--- src/plasticity/returnMapping.jl
+++ src/plasticity/returnMapping.jl
@@ -11,20 +11,9 @@ function checkPlasticState!(plasticVars::PlasticVars, model::PlasticModel,
     model.ℂ!(plasticVars.C, plasticVars.σ_voigt, plasticVars.q, plasticVars, params)
     plasticVars.σ_voigt = plasticVars.C*(plasticVars.ϵ .- plasticVars.ϵᵖ)
     model.𝓗!(plasticVars.H, plasticVars.σ_voigt, plasticVars.q, plasticVars.α, plasticVars, params)
-    #for i ∈ 1:length(plasticVars.q)
-    #    plasticVars.q[i] = -plasticVars.H[i]
-    #end
     plasticVars.q = -plasticVars.H
-    if model.𝒇(plasticVars.σ_voigt, plasticVars.q, plasticVars, params) > 0
-        #println("In plastic regime")
-        returnMapping!(plasticVars, model, params)
-        updateStateDict!(plasticVars.ϵᵖ, plasticVars.α, stateDictBuffer, elementNo, integrationPt)
-        return true
-    else
-        #println("In elastic regime")
-        plasticVars.Cᵀ = plasticVars.C
-        return false
-    end
+    returnMapping!(plasticVars, model, params)
+    updateStateDict!(plasticVars.ϵᵖ, plasticVars.α, stateDictBuffer, elementNo, integrationPt)
 end
 
 
@@ -36,13 +25,13 @@ function initReturnMappingVars(model::PlasticModel)
     ∂h_∂σ::Array{Float64, 2} = zeros(model.αSize, model.ϵSize)
     ∂h_∂q::Array{Float64, 2} = zeros(model.αSize, model.αSize)
     A::Array{Float64, 2} = zeros(model.ϵSize+model.αSize, model.ϵSize+model.αSize)
+    invA::Array{Float64, 2} = zeros(model.ϵSize+model.αSize, model.ϵSize+model.αSize)
     Θ::Array{Float64, 1} = zeros(model.ϵSize)
     h::Array{Float64, 1} = zeros(model.αSize)
     R::Array{Float64, 1} = ones(model.ϵSize+model.αSize)
-    f::Float64 = 1.0
     Δλ::Float64 = 0.0
     dΔλ::Float64 = 0.0
-    return ∂f_∂σ, ∂f_∂q, ∂Θ_∂σ, ∂Θ_∂q, ∂h_∂σ, ∂h_∂q, Θ, h, R, A, f, Δλ, dΔλ
+    return ∂f_∂σ, ∂f_∂q, ∂Θ_∂σ, ∂Θ_∂q, ∂h_∂σ, ∂h_∂q, Θ, h, R, A, invA, Δλ, dΔλ
 end
 
 function updateReturnMappingVars!(∂f_∂σ::Array{Float64, 1},
@@ -99,50 +88,52 @@ d(\\Delta\\lambda)``
 function returnMapping!(plasticVars::PlasticVars, model::PlasticModel,
     params::ModelParams)
 
-    ∂f_∂σ::Array{Float64, 1}, ∂f_∂q::Array{Float64, 1},
-    ∂Θ_∂σ::Array{Float64, 2}, ∂Θ_∂q::Array{Float64, 2},
-    ∂h_∂σ::Array{Float64, 2}, ∂h_∂q::Array{Float64, 2},
-    Θ::Array{Float64, 1}, h::Array{Float64, 1},
-    R::Array{Float64, 1}, A::Array{Float64, 2},
-    f::Float64, Δλ::Float64, dΔλ::Float64 = initReturnMappingVars(model)
-
-    iter::Int64 = 0
-    ϵᵖα_n1::Array{Float64, 1} = [plasticVars.ϵᵖ; plasticVars.α]
-    while ((norm(f)> tolerance.f|| norm(R)> tolerance.R) && iter<=tolerance.maxIter)
-        ϵₘ::Float64, plasticVars.𝒆ᵖ = get_ϵₘ_𝒆(ϵᵖα_n1[1:model.ϵSize])
-        plasticVars.σ_voigt = plasticVars.C*(plasticVars.ϵ - ϵᵖα_n1[1:model.ϵSize])
-        model.𝓗!(plasticVars.H, plasticVars.σ_voigt, plasticVars.q,
-        ϵᵖα_n1[model.ϵSize+1:model.ϵSize+model.αSize], plasticVars, params)
-        plasticVars.q = -plasticVars.H
-        f = model.𝒇(plasticVars.σ_voigt, plasticVars.q, plasticVars, params)
+    f::Float64 = model.𝒇(plasticVars.σ_voigt, plasticVars.q, plasticVars, params)
+    if (f> 0)
+        ∂f_∂σ, ∂f_∂q, ∂Θ_∂σ, ∂Θ_∂q, ∂h_∂σ, ∂h_∂q, Θ, h, R, A, invA, Δλ, dΔλ = initReturnMappingVars(model)
         #Update Return mapping internal arrays
         updateReturnMappingVars!(∂f_∂σ, ∂f_∂q, ∂Θ_∂σ, ∂Θ_∂q, ∂h_∂σ, ∂h_∂q, Θ, h, plasticVars, model, params)
         Θh::Array{Float64, 1} = [Θ; h]
-        #Update Residual
-        R = -ϵᵖα_n1 + [plasticVars.ϵᵖ; plasticVars.α] + Δλ*Θh
-        #println("-ϵᵖα_n1 ", -ϵᵖα_n1," Δλ*Θh = ", Δλ*Θh)
-        #update matrix [A]
-        A[1:model.ϵSize,1:model.ϵSize] = inv(plasticVars.C) + Δλ*∂Θ_∂σ
-        A[model.ϵSize+1:model.ϵSize+model.αSize, 1:model.ϵSize] = Δλ*∂h_∂σ
-        A[1:model.ϵSize, model.ϵSize+1:model.ϵSize+model.αSize] = Δλ*∂Θ_∂q
-        A[model.ϵSize+1:model.ϵSize+model.αSize, model.ϵSize+1:model.ϵSize+model.αSize] =
-        inv(plasticVars.D)+ Δλ*∂h_∂q
+        Δσ_Δq::Array{Float64, 1} = zeros(model.ϵSize+model.αSize)
+        ϵᵖα_n1::Array{Float64, 1} = [plasticVars.ϵᵖ; plasticVars.α]
+        iter::Int64 = 0
+        while ((f> tolerance.f|| norm(R)> tolerance.R) && iter<=tolerance.maxIter)
+            #update matrix [invA]
+            invA[1:model.ϵSize,1:model.ϵSize] =  inv(plasticVars.C) + Δλ*∂Θ_∂σ
+            invA[model.ϵSize+1:model.ϵSize+model.αSize, 1:model.ϵSize] = Δλ*∂h_∂σ
+            invA[1:model.ϵSize, model.ϵSize+1:model.ϵSize+model.αSize] = Δλ*∂Θ_∂q
+            invA[model.ϵSize+1:model.ϵSize+model.αSize, model.ϵSize+1:model.ϵSize+model.αSize] =
+            inv(plasticVars.D) + Δλ*∂h_∂q
 
-        fA = [∂f_∂σ..., ∂f_∂q...]'*A
-        dΔλ = (f - fA*R)/(fA*Θh)
-        Δλ += dΔλ
-        C_D_inv::Array{Float64, 2} = inv([(plasticVars.C) zeros(model.ϵSize, model.αSize);
-                                        zeros(model.αSize, model.ϵSize) (plasticVars.D)])
-        ϵᵖα_n1 +=C_D_inv*A*(R + dΔλ*Θh)
-        iter += 1
-        println("f = ", f, " norm(R) = ", norm(R), " dΔλ = ", dΔλ)
+            A = inv(invA)
+            fA = [∂f_∂σ..., ∂f_∂q...]'*A
+            dΔλ = (f - fA*R)/(fA*Θh)
+            Δλ += dΔλ
+            Δσ_Δq = -A*(R + dΔλ*Θh)
+            plasticVars.σ_voigt += Δσ_Δq[1:model.ϵSize]
+            plasticVars.q += Δσ_Δq[model.ϵSize+1:model.ϵSize+model.αSize]
+
+            C_D_inv::Array{Float64, 2} = [inv(plasticVars.C) zeros(model.ϵSize, model.αSize);
+                                            zeros(model.αSize, model.ϵSize) inv(plasticVars.D)]
+
+            ϵᵖα_n1 += -C_D_inv*Δσ_Δq
+            f = model.𝒇(plasticVars.σ_voigt, plasticVars.q, plasticVars, params)
+            #Update Return mapping internal arrays
+            updateReturnMappingVars!(∂f_∂σ, ∂f_∂q, ∂Θ_∂σ, ∂Θ_∂q, ∂h_∂σ, ∂h_∂q, Θ, h, plasticVars, model, params)
+            Θh = [Θ; h]
+            #Update Residual
+            R = -ϵᵖα_n1 + [plasticVars.ϵᵖ; plasticVars.α] + Δλ*Θh
+            iter += 1
+            println(" Δλ*Θh = ", Δλ*Θh)
+            println("f = ", f, " norm(R) = ", norm(R), " dΔλ = ", dΔλ)
+        end
+        if iter > tolerance.maxIter
+            @warn "Return Mapping Exited without convergence"
+        end
+        plasticVars.ϵᵖ = ϵᵖα_n1[1:model.ϵSize]
+        plasticVars.α = ϵᵖα_n1[model.ϵSize+1:model.ϵSize+model.αSize]
+        plasticVars.σ_voigt = plasticVars.C*(plasticVars.ϵ - plasticVars.ϵᵖ)
+        model.𝓗!(plasticVars.H, plasticVars.σ_voigt, plasticVars.q,
+        plasticVars.α, plasticVars, params)
     end
-    if iter > tolerance.maxIter
-        @warn "Return Mapping Exited without convergence"
-    end
-    plasticVars.ϵᵖ = ϵᵖα_n1[1:model.ϵSize]
-    plasticVars.α = ϵᵖα_n1[model.ϵSize+1:model.ϵSize+model.αSize]
-    plasticVars.σ_voigt = plasticVars.C*(plasticVars.ϵ - plasticVars.ϵᵖ)
-    model.𝓗!(plasticVars.H, plasticVars.σ_voigt, plasticVars.q,
-    plasticVars.α, plasticVars, params)
 end
